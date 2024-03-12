5510d7032f65b5180829334df3b7a4a8dda078b9
Michele <mikzed@hotmail.it>
2024-03-12 16:33:46 +0100

    deploy

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "../styles/portfolio.css";
 import WorkCard from "../components/WorkCard";
+import theHandGame from "../public/thehandgame.png";
 
 function Portfolio() {
   const [isMobileCompatible, setIsMobileCompatible] = useState(true);
@@ -19,7 +20,7 @@ function Portfolio() {
       id: 0,
       workTitle: "theHandGame",
       workDescription: "The iconic rock-paper-scissors game",
-      workImg: "/public/thehandgame.png",
+      workImg: { theHandGame },
       workLink: "https://thehandgame.netlify.app",
       isMobileCompatible: true,
     },
